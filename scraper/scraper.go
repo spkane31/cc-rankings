1bb7bf8760060ddf9265c4cd54a6b7e25d1deb3e
Sean Kane <spkane31@gmail.com>
2019-06-04 20:07:30 +0200

    beginnings of the golang scraper, can send a POST for month/year. next step = scrape a page

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,24 +1,83 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"net/http"
 	"log"
+	"net/url"
+	"strconv"
 	"strings"
-	"io/ioutil"
+	// "io/ioutil"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-func main() {
-	fmt.Println("Scraping TFRRS!")
-	response, err := http.Get("https://www.tfrrs.org/")
-	if err != nil {
-		log.Fatal(err)
+// var count int
+var links []string
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
 	}
-	fmt.Println(response)
+}
 
-	dataInBytes, err := ioutil.ReadAll(response.Body)
-	pageContent := string(dataInBytes)
+func processElement(index int, element *goquery.Selection) {
+	href, exists := element.Attr("href")
+	if exists {
+		// fmt.Println(href)
+		ParseUrl(href)
+	}
+}
 
-	titleStartIndex := strings.Index(pageContent, "<title>")
-	fmt.Println(titleStartIndex)
+func ParseUrl(s string) {
+	parsedUrl, err := url.Parse(s)
+	check(err)
+	path := parsedUrl.Path
+
+	if strings.Contains(path, "/results/") {
+		links = append(links, parsedUrl.Path)	
+	}
+}
+
+func GetUrlMonthYear(month, year int) {
+	y := strconv.Itoa(year)
+	m := strconv.Itoa(month)
+
+	response, err := http.PostForm(
+		"https://www.tfrrs.org/results_search.html",
+		url.Values{
+			"sport": {"xc"},
+			"state": {""},
+			"month": {m},
+			"year": {y},
+		},
+	)
+
+	check(err)
+	defer response.Body.Close()
+	document, err := goquery.NewDocumentFromReader(response.Body)
+	check(err)
+	document.Find("a").Each(processElement)
+}
+
+func ScrapePage(link string) {
+	// This function will scrape a race page and create the CSV, and the 
+}
+
+func main() {
+	
+	log.Println("Scraping TFRRS!")
+	GetUrlMonthYear(11, 2018)
+	log.Printf("Found %d Links!", len(links))
+	// log.Println(links)
+	response, err := http.Get("https://www.tfrrs.org/results_search.html")
+	check(err)
+	defer response.Body.Close()
+
+	// fmt.Println(response)
+
+	// document, err := goquery.NewDocumentFromReader(response.Body)
+	// check(err)
+
+	// document.Find("a").Each(processElement)
 }
