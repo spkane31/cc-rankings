5c5a209560da184e07c256d8004d38f4fc7a9b31
Sean Kane <spkane31@gmail.com>
2019-06-10 15:44:48 +0200

    added races table

---
+++ rankings/lib/rankings/race.ex
@@ -0,0 +1,19 @@
+defmodule Rankings.Race do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "races" do
+    field :name, :string
+    field :course, :string
+    field :distance, :integer
+    field :gender, :string
+    field :correction, :float
+  end
+
+  def changeset(struct, params) do
+    struct
+    |> cast(params, [:name])
+    |> validate_required([:name])
+    |> validate_length(:name, min: 1)
+  end
+end
---
+++ rankings/priv/repo/migrations/20190610133725_create_races.exs
@@ -0,0 +1,13 @@
+defmodule Rankings.Repo.Migrations.CreateRaces do
+  use Ecto.Migration
+
+  def change do
+    create table(:races) do
+      add :name, :string, null: false
+      add :course, :string, null: false
+      add :distance, :integer
+      add :gender, :string
+      add :correction, :float
+    end
+  end
+end
