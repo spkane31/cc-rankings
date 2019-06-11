7ca99af065c8ac4c10a68a4ba67ebf9fb1897543
Sean Kane <spkane31@gmail.com>
2019-06-11 22:19:20 +0200

    added to the table, the team information for results :show page

--- rankings/lib/rankings_web/views/raceinstance_view.ex
+++ rankings/lib/rankings_web/views/raceinstance_view.ex
@@ -3,6 +3,7 @@ defmodule RankingsWeb.RaceInstanceView do
 
   alias Rankings.RaceInstance
   alias Rankings.Runner
+  alias Rankings.Team
 
   def get_date(%RaceInstance{date: date}) do
     date
@@ -20,6 +21,12 @@ defmodule RankingsWeb.RaceInstanceView do
   def get_runner_name(id) do
     runner = Repo.get(Runner, id)
     runner.first_name <> " " <> runner.last_name
+  end
+
+  def get_runner_team(id) do
+    runner = Repo.get(Runner, id) |> Repo.preload(:team)
+    team = Repo.get(Team, runner.team_id)
+    team.name
   end
 
 end
