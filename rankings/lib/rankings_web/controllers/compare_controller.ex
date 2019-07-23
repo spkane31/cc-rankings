48db7f3d2a1da87926bed5702ebefeaff0c6beae
Sean Kane <spkane31@gmail.com>
2019-07-23 16:55:26 +0200

    got it working for one runner, now going to try to mess with the router

--- rankings/lib/rankings_web/controllers/compare_controller.ex
+++ rankings/lib/rankings_web/controllers/compare_controller.ex
@@ -1,20 +1,36 @@
 defmodule RankingsWeb.CompareController do
   use RankingsWeb, :controller
+  import Logger
 
-  alias Rankings.Runner
+  alias Rankings.{Runner, Repo}
 
   def index(conn, params) do
-    first = get_in(params, ["first"])
-    last = get_in(params, ["last"])
+    # first = get_in(params, ["first"])
+    # last = get_in(params, ["last"])
+
     query = Runner.list_runners(params)
-    render(conn, "index.html", runners: query)
+
+    id1 = get_in(params, ["id1"])
+    id2 = get_in(params, ["id2"])
+
+    runner1 = find_runner(id1)
+    runner2 = find_runner(id2)
+
+    render(conn, "index.html", runners: query, id1: id1, id2: id2, runner1: runner1)
   end
 
-  def show(conn, %{"id1" => id1}) do
-    render(conn, "index.html", runners: nil, id1: id1)
+  def show(conn, params) do
+    id1 = get_in(params, ["id1"])
+    id2 = get_in(params, ["id2"])
+    render(conn, "index.html", id1: id1, id1: id2)
   end
 
-  def show(conn, %{"id1" => id1, "id2" => id2}) do
-    render(conn, "show.html", id1: id1, id2: id2)
+  def find_runner(id) do
+    if id == nil do
+      nil
+    else
+      Runner.get_runner(id) |> Repo.preload(:team)
+    end
   end
+
 end
